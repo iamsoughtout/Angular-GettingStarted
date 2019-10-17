3b063964b1addbdb74bf633d48e448a1bed0e712
etim uwem <soughtoutonlyme@gmail.com>
2019-10-17 03:53:52 +0100

    created app component and app module; created ProductComponent

---
+++ APM/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';   //
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+
+// NOTE: the properties of an NgModule are arrays
+@NgModule({
+
+  // DECLARATIONS:
+  // used to declare which application components belong to this module.
+  // By convention the AppComponent belongs here.
+  // Other components can be added here as well.
+
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+
+  // IMPORTS:
+  // used to import external modules into the components listed here
+  imports: [
+    BrowserModule
+  ],
+
+  // BOOTSTRAP:
+  // used to declare the AppComponent i.e. app.component.ts, as the startup component of the application.
+  // the startup component must contain the selector which has the directive that is used in the index.html file.
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {
+
+}
+
